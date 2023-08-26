0753c46a50bca37eab2bfce67fb61197261cabcf
MRIiiIiIiI <142177107+MRIiiIiIiI@users.noreply.github.com>
2023-08-26 00:57:11 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,16 @@
+from analyzer import *
 
-from analyzer import IPAnalyzer, extract_ip
+question = input("Do you want to know the IP address or make groups? ")
 
-user_input = input(">> Enter your IP address (e.g., 192.168.1.1): ")
-ip_parts = extract_ip(user_input)
-
-if ip_parts is None:
-    print("Invalid IP address format.")
-else:
+if question.lower() == "know ip":
+    user_input = input(">> Enter your IP address (e.g., 192.168.1.1): ")
+    ip_parts = extract_ip(user_input)
+    if ip_parts is None:
+        print("Invalid IP address format.")
     ip_analyzer = IPAnalyzer(ip_parts)
     ip_analyzer.analyze_ip()
+elif question.lower() == "make groups":
+    dividing_instance = Dividing()
+    dividing_instance.run()
+else:
+    print("Invalid choice.")
